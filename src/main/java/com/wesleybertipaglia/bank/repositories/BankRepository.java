c761e216270b80ec048be96c2b400d8f06c4302d
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-29 20:36:55 -0300

    feat: add findByCode and existsByCode in bank repository

--- src/main/java/com/wesleybertipaglia/bank/repositories/BankRepository.java
+++ src/main/java/com/wesleybertipaglia/bank/repositories/BankRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.wesleybertipaglia.bank.models.Bank;
 
 public interface BankRepository extends JpaRepository<Bank, UUID> {
+    Bank findByCode(String code);
+
+    boolean existsByCode(String code);
 }
