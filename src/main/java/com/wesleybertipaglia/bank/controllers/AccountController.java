9b67b87fe97514cbbf91f022e0a1118e2af304cc
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-29 21:58:08 -0300

    chore: remove unused import in AccountController

--- src/main/java/com/wesleybertipaglia/bank/controllers/AccountController.java
+++ src/main/java/com/wesleybertipaglia/bank/controllers/AccountController.java
@@ -1,7 +1,6 @@
 package com.wesleybertipaglia.bank.controllers;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
