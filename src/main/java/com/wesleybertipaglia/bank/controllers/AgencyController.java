1a5b840a803a5dccc8cc7936a349857ba415ade2
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-30 12:42:47 -0300

    style: refactor AgencyController

--- src/main/java/com/wesleybertipaglia/bank/controllers/AgencyController.java
+++ src/main/java/com/wesleybertipaglia/bank/controllers/AgencyController.java
@@ -1,53 +1,47 @@
 package com.wesleybertipaglia.bank.controllers;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.wesleybertipaglia.bank.dtos.AgencyDTO;
-import com.wesleybertipaglia.bank.models.Agency;
 import com.wesleybertipaglia.bank.services.AgencyService;
 
 @RestController
-@RequestMapping("/agencies")
+@RequestMapping("/api/v1/agencies")
 public class AgencyController {
 
     @Autowired
     private AgencyService agencyService;
 
     @PostMapping("/")
-    public AgencyDTO createAgency(@RequestParam UUID bankId, @RequestBody Agency agency) {
-        return agencyService.createAgency(bankId, agency);
+    public ResponseEntity<AgencyDTO> createAgency(@RequestBody AgencyDTO agencyDTO) {
+        return ResponseEntity.of(agencyService.createAgency(agencyDTO));
     }
 
     @GetMapping("/")
-    public List<AgencyDTO> listAgencies() {
-        return agencyService.listAgencies();
+    public ResponseEntity<Page<AgencyDTO>> listAgencies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(agencyService.listAgencies(page, size));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<AgencyDTO> getAgencyById(@PathVariable UUID id) {
-        Optional<AgencyDTO> agency = agencyService.getAgencyById(id);
-        return agency.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return ResponseEntity.of(agencyService.getAgencyById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<AgencyDTO> updateAgency(@PathVariable UUID id, @RequestBody Agency agency) {
-        Optional<AgencyDTO> updatedAgency = agencyService.updateAgency(id, agency);
-        return updatedAgency.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<AgencyDTO> updateAgency(@PathVariable UUID id, @RequestBody AgencyDTO agencyDTO) {
+        return ResponseEntity.of(agencyService.updateAgency(id, agencyDTO));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAgency(@PathVariable UUID id) {
-        try {
-            agencyService.deleteAgency(id);
-            return ResponseEntity.noContent().build();
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<String> deleteAgency(@PathVariable UUID id) {
+        return ResponseEntity.of(agencyService.deleteAgency(id));
     }
+
 }
