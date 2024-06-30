c0696d854a4c099cdf91fbd6ae4dbe45ab9cfe8f
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-30 10:44:36 -0300

    feat: add exceptions to BankMapper

--- src/main/java/com/wesleybertipaglia/bank/mappers/BankMapper.java
+++ src/main/java/com/wesleybertipaglia/bank/mappers/BankMapper.java
@@ -4,12 +4,31 @@ import com.wesleybertipaglia.bank.dtos.BankDTO;
 import com.wesleybertipaglia.bank.models.Bank;
 
 public class BankMapper {
-
     public static BankDTO toDTO(Bank bank) {
+        if (bank == null) {
+            return null;
+        } else if (bank.getId() == null) {
+            throw new IllegalArgumentException("Bank ID is required");
+        } else if (bank.getName() == null) {
+            throw new IllegalArgumentException("Bank name is required");
+        } else if (bank.getCode() == null) {
+            throw new IllegalArgumentException("Bank code is required");
+        }
+
         return new BankDTO(bank.getId(), bank.getName(), bank.getCode());
     }
 
     public static Bank toEntity(BankDTO dto) {
+        if (dto == null) {
+            return null;
+        } else if (dto.getId() == null) {
+            throw new IllegalArgumentException("Bank ID is required");
+        } else if (dto.getName() == null) {
+            throw new IllegalArgumentException("Bank name is required");
+        } else if (dto.getCode() == null) {
+            throw new IllegalArgumentException("Bank code is required");
+        }
+
         Bank bank = new Bank();
         bank.setId(dto.getId());
         bank.setName(dto.getName());
