233e586db2601bcd34ee97082d3cf4366bca0929
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-30 12:42:31 -0300

    style: refactor AgencyService

--- src/main/java/com/wesleybertipaglia/bank/services/AgencyService.java
+++ src/main/java/com/wesleybertipaglia/bank/services/AgencyService.java
@@ -1,68 +1,82 @@
 package com.wesleybertipaglia.bank.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wesleybertipaglia.bank.dtos.AgencyDTO;
+import com.wesleybertipaglia.bank.mappers.AgencyMapper;
 import com.wesleybertipaglia.bank.models.Agency;
 import com.wesleybertipaglia.bank.models.Bank;
 import com.wesleybertipaglia.bank.repositories.AgencyRepository;
 import com.wesleybertipaglia.bank.repositories.BankRepository;
 
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class AgencyService {
+
     @Autowired
     private AgencyRepository agencyRepository;
 
     @Autowired
     private BankRepository bankRepository;
 
-    public List<AgencyDTO> listAgencies() {
-        return agencyRepository.findAll().stream().map(this::convertToDTO).collect(Collectors.toList());
-    }
+    @Transactional
+    public Optional<AgencyDTO> createAgency(AgencyDTO agencyDTO) {
+        if (agencyRepository.existsByNumberAndBankId(agencyDTO.getNumber(), agencyDTO.getBankId())) {
+            throw new EntityExistsException("Agency number already exists");
+        }
 
-    public Optional<AgencyDTO> getAgencyById(UUID id) {
-        return agencyRepository.findById(id).map(this::convertToDTO);
-    }
-
-    public AgencyDTO createAgency(UUID bankId, Agency agency) {
-        Bank bank = bankRepository.findById(bankId)
+        Bank bank = bankRepository.findById(agencyDTO.getBankId())
                 .orElseThrow(() -> new IllegalArgumentException("Bank not found"));
 
-        agency.setBank(bank);
-        Agency savedAgency = agencyRepository.save(agency);
-        return convertToDTO(savedAgency);
+        Agency agency = new Agency(agencyDTO.getNumber(), agencyDTO.getAddress(), bank);
+        return Optional.of(AgencyMapper.convertToDTO(agencyRepository.save(agency)));
     }
 
-    public Optional<AgencyDTO> updateAgency(UUID id, Agency agency) {
-        return agencyRepository.findById(id).map(existingAgency -> {
-            existingAgency.setNumber(agency.getNumber());
-            existingAgency.setAddress(agency.getAddress());
-            existingAgency.setBank(agency.getBank());
-            Agency updatedAgency = agencyRepository.save(existingAgency);
-            return convertToDTO(updatedAgency);
-        });
+    @Transactional(readOnly = true)
+    public Page<AgencyDTO> listAgencies(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return agencyRepository.findAll(pageable).map(AgencyMapper::convertToDTO);
     }
 
-    public void deleteAgency(UUID id) {
-        if (agencyRepository.existsById(id)) {
-            agencyRepository.deleteById(id);
-        } else {
-            throw new IllegalArgumentException("Agency not found");
+    @Transactional(readOnly = true)
+    public Optional<AgencyDTO> getAgencyById(UUID id) {
+        if (!agencyRepository.existsById(id)) {
+            throw new EntityNotFoundException("Agency not found");
         }
+
+        return Optional.of(AgencyMapper.convertToDTO(agencyRepository.findById(id).get()));
     }
 
-    private AgencyDTO convertToDTO(Agency agency) {
-        AgencyDTO dto = new AgencyDTO();
-        dto.setId(agency.getId());
-        dto.setNumber(agency.getNumber());
-        dto.setAddress(agency.getAddress());
-        dto.setBankId(agency.getBank().getId());
-        return dto;
+    @Transactional
+    public Optional<AgencyDTO> updateAgency(UUID id, AgencyDTO agencyDTO) {
+        Agency storedAgency = agencyRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Agency not found"));
+
+        Bank bank = bankRepository.findById(agencyDTO.getBankId())
+                .orElseThrow(() -> new IllegalArgumentException("Bank not found"));
+
+        storedAgency.setNumber(agencyDTO.getNumber());
+        storedAgency.setAddress(agencyDTO.getAddress());
+        storedAgency.setBank(bank);
+
+        return Optional.of(AgencyMapper.convertToDTO(agencyRepository.save(storedAgency)));
     }
+
+    public Optional<String> deleteAgency(UUID id) {
+        Agency storedAgency = agencyRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Agency not found"));
+
+        agencyRepository.delete(storedAgency);
+        return Optional.of("Agency deleted successfully");
+    }
+
 }
