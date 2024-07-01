9076753ce261e6678700c076a0d8de5766ea0d0d
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-01 10:39:46 -0300

    feat: add findByAccountId to TransactionRepository

--- src/main/java/com/wesleybertipaglia/bank/repositories/TransactionRepository.java
+++ src/main/java/com/wesleybertipaglia/bank/repositories/TransactionRepository.java
@@ -1,9 +1,12 @@
 package com.wesleybertipaglia.bank.repositories;
 
+import java.util.List;
 import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wesleybertipaglia.bank.models.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    List<Transaction> findByAccountId(UUID accountId);
 }
