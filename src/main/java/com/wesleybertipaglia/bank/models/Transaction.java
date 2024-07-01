a142b85be986fa1124a804693485132458f36fb9
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-01 10:39:24 -0300

    feat: add status and constructors to Transaction

--- src/main/java/com/wesleybertipaglia/bank/models/Transaction.java
+++ src/main/java/com/wesleybertipaglia/bank/models/Transaction.java
@@ -6,6 +6,9 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.wesleybertipaglia.bank.enums.TransactionStatus;
+import com.wesleybertipaglia.bank.enums.TransactionType;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -33,11 +36,33 @@ public class Transaction {
 
     private String description;
 
+    private TransactionStatus status;
+
     @CreationTimestamp
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+    public Transaction() {
+    }
+
+    public Transaction(UUID id, float value, TransactionType type, TransactionStatus status, String description) {
+        this.id = id;
+        this.value = value;
+        this.type = type;
+        this.status = status;
+        this.description = description;
+    }
+
+    public Transaction(float value, TransactionType type, TransactionStatus status, String description,
+            Account account) {
+        this.value = value;
+        this.type = type;
+        this.status = status;
+        this.description = description;
+        this.account = account;
+    }
 
     public UUID getId() {
         return id;
@@ -77,6 +102,14 @@ public class Transaction {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
     }
 
     public LocalDateTime getCreatedAt() {
