ae6fbc85d01f6365020f734d34a29c1c38ca3949
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-01 12:37:02 -0300

    feat: add constructors to TransferDTO

--- src/main/java/com/wesleybertipaglia/bank/dtos/TransferDTO.java
+++ src/main/java/com/wesleybertipaglia/bank/dtos/TransferDTO.java
@@ -7,6 +7,30 @@ public class TransferDTO {
     private UUID accountSourceId;
     private UUID accountDestinationId;
     private float value;
+    private String type;
+    private String status;
+
+    public TransferDTO() {
+        this.type = "TRANSFER";
+        this.status = "PENDING";
+    }
+
+    public TransferDTO(UUID accountSourceId, UUID accountDestinationId, float value) {
+        this.accountSourceId = accountSourceId;
+        this.accountDestinationId = accountDestinationId;
+        this.value = value;
+        this.type = "TRANSFER";
+        this.status = "PENDING";
+    }
+
+    public TransferDTO(UUID id, UUID accountSourceId, UUID accountDestinationId, float value, String status) {
+        this.id = id;
+        this.accountSourceId = accountSourceId;
+        this.accountDestinationId = accountDestinationId;
+        this.value = value;
+        this.type = "TRANSFER";
+        this.status = status;
+    }
 
     public UUID getId() {
         return id;
@@ -39,4 +63,21 @@ public class TransferDTO {
     public void setValue(float value) {
         this.value = value;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
